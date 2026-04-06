97223df0901898812ab8776f8ceac4637c575b91
Galih Praz <pendtiumpraz@gmail.com>
2026-04-06 15:06:47 +0700

    fix migration

--- database/migrations/2026_04_06_071002_add_locale_to_users_table.php
+++ database/migrations/2026_04_06_071002_add_locale_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('locale')->default('id')->after('theme_preference');
+            $table->string('locale')->default('id')->nullable();
         });
     }
 
